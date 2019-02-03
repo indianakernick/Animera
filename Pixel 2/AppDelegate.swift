0f10a0833739268856c334e846bf313c3d39de78
Kerndog73 <kerndog73@gmail.com>
2019-02-03 12:57:22 +1030

    Initial Commit

---
+++ Pixel 2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pixel 2
+//
+//  Created by Indi Kernick on 3/2/19.
+//  Copyright © 2019 Indi Kernick. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
