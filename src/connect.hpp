c08dbf56774e2d8eb35cde051540bc3dab580f7b
Kerndog73 <kerndog73@gmail.com>
2019-07-31 10:22:24 +0930

    Remove evil macros

--- src/connect.hpp
+++ src/connect.hpp
@@ -32,13 +32,6 @@ constexpr auto value(T &&obj, int) -> std::decay_t<decltype(*obj)>;
 template <typename T>
 constexpr auto value(T &&obj, long) -> std::decay_t<decltype(obj)>;
 
-template <typename Object>
-constexpr auto setter(Object *obj) {
-  return [obj](auto &&value) {
-    *obj = std::forward<decltype(value)>(value);
-  };
-}
-
 }
 
 #define CONNECT(SENDER, SIGNAL, RECEIVER, SLOT)                                 \
@@ -56,22 +49,12 @@ constexpr auto setter(Object *obj) {
     __VA_ARGS__                                                                 \
   )
 
-#define CONNECT_SETTER(SENDER, SIGNAL, RECEIVER)                                \
-  CONNECT_LAMBDA(SENDER, SIGNAL, detail::setter(detail::address(RECEIVER, 0)))
-
 #define CONNECT_OVERLOAD(SENDER, SIGNAL, RECEIVER, SLOT, ...)                   \
   QObject::connect(                                                             \
     detail::address(SENDER, 0),                                                 \
     qOverload<__VA_ARGS__>(&decltype(detail::value(SENDER, 0))::SIGNAL),        \
     detail::address(RECEIVER, 0),                                               \
     qOverload<__VA_ARGS__>(&decltype(detail::value(RECEIVER, 0))::SLOT)         \
-  )
-
-#define CONNECT_SETTER_OVERLOAD(SENDER, SIGNAL, RECEIVER, ...)                  \
-  QObject::connect(                                                             \
-    detail::address(SENDER, 0),                                                 \
-    qOverload<__VA_ARGS__>(&decltype(detail::value(SENDER, 0))::SIGNAL),        \
-    detail::setter(detail::address(RECEIVER, 0))                                \
   )
 
 // connect\((.+),\s+&\w+::(\w+),\s+(.+),\s+&\w+::(\w+)\);
