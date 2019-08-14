8ed09997a720f85e3c80b8687fa14f0b17190c77
Kerndog73 <kerndog73@gmail.com>
2019-08-14 14:38:32 +0930

    Refactor ChunkWriter

--- src/serial.cpp
+++ src/serial.cpp
@@ -7,3 +7,63 @@
 //
 
 #include "serial.hpp"
+
+#include <QtCore/qendian.h>
+
+ChunkWriter::ChunkWriter(QIODevice &dev)
+  : dev{dev} {}
+
+void ChunkWriter::begin(const uint32_t len, const char *header) {
+  startPos = -1;
+  writeHeader(len, header);
+}
+
+void ChunkWriter::begin(const char *header) {
+  assert(!dev.isSequential());
+  startPos = dev.pos();
+  writeHeader(0, header);
+}
+
+void ChunkWriter::end() {
+  if (startPos != -1) {
+    const qint64 currPos = dev.pos();
+    const uint32_t dataLen = static_cast<uint32_t>(currPos - startPos - 8);
+    dev.seek(startPos);
+    writeInt(dataLen);
+    dev.seek(currPos);
+  }
+  writeInt(static_cast<uint32_t>(crc));
+}
+
+void ChunkWriter::writeByte(const uint8_t byte) {
+  writeData(&byte);
+}
+
+void ChunkWriter::writeInt(uint32_t num) {
+  num = qToBigEndian(num);
+  writeData(&num);
+}
+
+void ChunkWriter::writeString(const char *dat, const uint32_t len) {
+  writeData(dat, len);
+}
+
+void ChunkWriter::writeString(const signed char *dat, const uint32_t len) {
+  writeData(dat, len);
+}
+
+void ChunkWriter::writeString(const unsigned char *dat, const uint32_t len) {
+  writeData(dat, len);
+}
+
+void ChunkWriter::writeHeader(const uint32_t len, const char *header) {
+  writeInt(len);
+  crc = crc32(0, nullptr, 0);
+  writeString(header, 4);
+}
+
+template <typename T>
+void ChunkWriter::writeData(const T *dat, const uint32_t len) {
+  dev.write(reinterpret_cast<const char *>(dat), len);
+  crc = crc32(crc, reinterpret_cast<const Bytef *>(dat), len);
+}
--- src/serial.hpp
+++ src/serial.hpp
@@ -10,15 +10,7 @@
 #define serial_hpp
 
 #include "zlib.hpp"
-#include <QtCore/qendian.h>
 #include <QtCore/qiodevice.h>
-
-template <typename T>
-void serializeBytes(QIODevice *dev, const T &data) {
-  assert(dev);
-  assert(dev->isWritable());
-  dev->write(reinterpret_cast<const char *>(&data), sizeof(T));
-}
 
 template <typename T>
 void deserializeBytes(QIODevice *dev, T &data) {
@@ -36,69 +28,27 @@ T deserializeBytesAs(QIODevice *dev) {
 
 class ChunkWriter {
 public:
-  explicit ChunkWriter(QIODevice &dev)
-    : dev{dev} {}
+  explicit ChunkWriter(QIODevice &);
 
-  void begin(const uint32_t len, const char *header) {
-    startPos = -1;
-    writeHeader(len, header);
-  }
+  void begin(uint32_t, const char *);
+  void begin(const char *);
+  void end();
   
-  void begin(const char *header) {
-    assert(!dev.isSequential());
-    startPos = dev.pos();
-    writeHeader(0, header);
-  }
-  
-  void writeByte(const uint8_t byte) {
-    writeToDev(&byte);
-    updateCRC(&byte);
-  }
-  
-  void writeInt(uint32_t num) {
-    num = qToBigEndian(num);
-    writeToDev(&num);
-    updateCRC(&num);
-  }
-  
-  template <typename Char>
-  std::enable_if_t<sizeof(Char) == 1 && std::is_integral_v<Char>>
-  writeString(const Char *dat, const uint32_t len) {
-    writeToDev(dat, len);
-    updateCRC(dat, len);
-  }
-  
-  void end() {
-    if (startPos != -1) {
-      const qint64 currPos = dev.pos();
-      const uint32_t dataLen = static_cast<uint32_t>(currPos - startPos - 8);
-      dev.seek(startPos);
-      writeInt(dataLen);
-      dev.seek(currPos);
-    }
-    writeInt(static_cast<uint32_t>(crc));
-  }
+  void writeByte(uint8_t);
+  void writeInt(uint32_t);
+  void writeString(const char *, uint32_t);
+  void writeString(const signed char *, uint32_t);
+  void writeString(const unsigned char *, uint32_t);
 
 private:
   QIODevice &dev;
   uLong crc;
   qint64 startPos;
   
-  void writeHeader(const uint32_t len, const char *header) {
-    writeInt(len);
-    crc = crc32(0, nullptr, 0);
-    writeString(header, 4);
-  }
+  void writeHeader(uint32_t, const char *);
   
   template <typename T>
-  void writeToDev(const T *dat, const uint32_t len = sizeof(T)) {
-    dev.write(reinterpret_cast<const char *>(dat), len);
-  }
-  
-  template <typename T>
-  void updateCRC(const T *dat, const uint32_t len = sizeof(T)) {
-    crc = crc32(crc, reinterpret_cast<const Bytef *>(dat), len);
-  }
+  void writeData(const T *, uint32_t = sizeof(T));
 };
 
 #endif
