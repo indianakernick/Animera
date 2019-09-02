447a15d2b68d312715cfdf8cf9587c042c31147f
Kerndog73 <kerndog73@gmail.com>
2019-09-02 19:08:06 +0930

    Add copy to Graphics library

---
+++ include/Graphics/copy.hpp
@@ -0,0 +1,47 @@
+//
+//  copy.hpp
+//  Animera
+//
+//  Created by Indi Kernick on 2/9/19.
+//  Copyright © 2019 Indi Kernick. All rights reserved.
+//
+
+#ifndef graphics_copy_hpp
+#define graphics_copy_hpp
+
+#include "traits.hpp"
+#include "surface.hpp"
+
+namespace gfx {
+
+template <typename Pixel>
+void copyRegion(
+  Surface<Pixel> dst,
+  CSurface<identity_t<Pixel>> src,
+  const Point srcPos
+) noexcept {
+  const Rect srcRect = {srcPos, src.size()};
+  const Rect dstRect = srcRect.intersected(dst.rect());
+  if (dstRect.empty()) return;
+  const size_t width = dstRect.s.w * sizeof(Pixel);
+  auto srcRowIter = range(src, {dstRect.p - srcPos, dstRect.s}).begin();
+  for (auto row : range(dst, dstRect)) {
+    std::memcpy(row.begin(), srcRowIter.begin(), width);
+    ++srcRowIter;
+  }
+}
+
+template <typename Pixel>
+void copy(Surface<Pixel> dst, CSurface<identity_t<Pixel>> src) noexcept {
+  assert(dst.size() == src.size());
+  const size_t width = dst.width() * sizeof(Pixel);
+  auto srcRowIter = range(src).begin();
+  for (auto row : range(dst)) {
+    std::memcpy(row.begin(), srcRowIter.begin(), width);
+    ++srcRowIter;
+  }
+}
+
+}
+
+#endif
