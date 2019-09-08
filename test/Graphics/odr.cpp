ab1ffb7831273009c39434f6ef49dc830f37c7ec
Kerndog73 <kerndog73@gmail.com>
2019-09-08 09:39:51 +0930

    Add ODR file
    Ensures that the header-only library doesn't break the One Definition Rule

---
+++ test/Graphics/odr.cpp
@@ -0,0 +1,21 @@
+//
+//  odr.cpp
+//  Graphics-Test
+//
+//  Created by Indi Kernick on 8/9/19.
+//  Copyright © 2019 Indi Kernick. All rights reserved.
+//
+
+#include <Graphics/surface.hpp>
+#include <Graphics/geometry.hpp>
+#include <Graphics/traits.hpp>
+#include <Graphics/range.hpp>
+#include <Graphics/color.hpp>
+#include <Graphics/iterator.hpp>
+#include <Graphics/format.hpp>
+#include <Graphics/porter duff.hpp>
+#include <Graphics/each.hpp>
+#include <Graphics/copy.hpp>
+#include <Graphics/fill.hpp>
+#include <Graphics/flood fill.hpp>
+#include <Graphics/transform.hpp>
