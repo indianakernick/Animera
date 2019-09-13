c2e61970f2c29de24d92957d9d93229c324c2cb9
Kerndog73 <kerndog73@gmail.com>
2019-09-13 13:23:31 +0930

    Add tests for transform
    I really throught I'd find a bug in the rotate function but it seems to be fine!

---
+++ test/Graphics/transform.cpp
@@ -0,0 +1,192 @@
+//
+//  transform.cpp
+//  Graphics-Test
+//
+//  Created by Indi Kernick on 13/9/19.
+//  Copyright © 2019 Indi Kernick. All rights reserved.
+//
+
+#include "../catch.hpp"
+#include <Graphics/transform.hpp>
+
+namespace {
+
+using Px = uint16_t;
+
+constexpr Px init = 0xFEDC;
+
+Px fn(const int x, const int y) {
+  return static_cast<Px>((x + 1) * 3 + (y + 3) * 5);
+}
+
+}
+
+TEST_CASE("transform") {
+  constexpr int width = 7;
+  constexpr int height = 5;
+  
+  Px dstArr[height][width];
+  Px srcArr[height][width];
+  for (int y = 0; y != height; ++y) {
+    for (int x = 0; x != width; ++x) {
+      dstArr[y][x] = init;
+    }
+  }
+  for (int y = 0; y != height; ++y) {
+    for (int x = 0; x != width; ++x) {
+      srcArr[y][x] = fn(x, y);
+    }
+  }
+  
+  SECTION("pixelTransform") {
+    gfx::Surface<Px> dst{&dstArr[0][0], width, width, height};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::pixelTransform(dst, src, [](Px px) {
+      return px + 42;
+    });
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dstArr[y][x] == fn(x, y) + 42);
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+  
+  SECTION("flipVert") {
+    gfx::Surface<Px> dst{&dstArr[0][0], width, width, height};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::flipVert(dst, src);
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dstArr[y][x] == fn(x, height - 1 - y));
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+  
+  SECTION("flipHori") {
+    gfx::Surface<Px> dst{&dstArr[0][0], width, width, height};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::flipHori(dst, src);
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dstArr[y][x] == fn(width - 1 - x, y));
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+  
+  SECTION("rotate1") {
+    gfx::Surface<Px> dst{&dstArr[0][0], height, height, width};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::rotate1(dst, src);
+    for (int y = 0; y != width; ++y) {
+      for (int x = 0; x != height; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dst.ref(x, y) == fn(y, height - 1 - x));
+      }
+    }
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+  
+  SECTION("rotate2") {
+    gfx::Surface<Px> dst{&dstArr[0][0], width, width, height};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::rotate2(dst, src);
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dstArr[y][x] == fn(width - 1 - x, height - 1 - y));
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+  
+  SECTION("rotate3") {
+    gfx::Surface<Px> dst{&dstArr[0][0], height, height, width};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::rotate3(dst, src);
+    for (int y = 0; y != width; ++y) {
+      for (int x = 0; x != height; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dst.ref(x, y) == fn(width - 1 - y, x));
+      }
+    }
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+  
+  SECTION("rotate(8) = copy") {
+    gfx::Surface<Px> dst{&dstArr[0][0], width, width, height};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::rotate(dst, src, 8);
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dstArr[y][x] == fn(x, y));
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+  
+  SECTION("rotate(-7) = rotate1") {
+    gfx::Surface<Px> dst{&dstArr[0][0], height, height, width};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::rotate(dst, src, -7);
+    for (int y = 0; y != width; ++y) {
+      for (int x = 0; x != height; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dst.ref(x, y) == fn(y, height - 1 - x));
+      }
+    }
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+  
+  SECTION("rotate(6) = rotate2") {
+    gfx::Surface<Px> dst{&dstArr[0][0], width, width, height};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::rotate(dst, src, 6);
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dstArr[y][x] == fn(width - 1 - x, height - 1 - y));
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+  
+  SECTION("rotate(-5) = rotate3") {
+    gfx::Surface<Px> dst{&dstArr[0][0], height, height, width};
+    gfx::CSurface<Px> src{&srcArr[0][0], width, width, height};
+    gfx::rotate(dst, src, -5);
+    for (int y = 0; y != width; ++y) {
+      for (int x = 0; x != height; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(dst.ref(x, y) == fn(width - 1 - y, x));
+      }
+    }
+    for (int y = 0; y != height; ++y) {
+      for (int x = 0; x != width; ++x) {
+        INFO("(" << x << ", " << y << ")");
+        REQUIRE(srcArr[y][x] == fn(x, y));
+      }
+    }
+  }
+}
