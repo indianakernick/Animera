5a129881906bd23b7b30669e76f1a3ca25c29d40
Kerndog73 <kerndog73@gmail.com>
2019-09-14 13:45:09 +0930

    Visit pixels as well as surfaces

--- include/Graphics/visit.hpp
+++ include/Graphics/visit.hpp
@@ -45,15 +45,23 @@ CSurface<Pixel> makeCSurface(const Image &image) noexcept {
 
 namespace detail {
 
+template <typename Pixel, typename Arg>
+auto handleArg(Arg &&arg) noexcept {
+  if constexpr (std::is_integral_v<std::decay_t<Arg>>) {
+    return static_cast<Pixel>(arg);
+  } else {
+    return makeSurface<Pixel>(std::forward<Arg>(arg));
+  }
+}
+
 template <typename Tuple, size_t... Is>
 [[nodiscard]] decltype(auto) visitSurfacesHelper(Tuple tuple, std::index_sequence<Is...>) {
   using Func = std::tuple_element_t<sizeof...(Is), Tuple>;
-  Func func = std::get<sizeof...(Is)>(tuple);
   switch (std::get<0>(tuple).depth()) {
-    case 8: return std::forward<Func>(func)(makeSurface<uint64_t>(std::forward<std::tuple_element_t<Is, Tuple>>(std::get<Is>(tuple)))...);
-    case 4: return std::forward<Func>(func)(makeSurface<uint32_t>(std::forward<std::tuple_element_t<Is, Tuple>>(std::get<Is>(tuple)))...);
-    case 2: return std::forward<Func>(func)(makeSurface<uint16_t>(std::forward<std::tuple_element_t<Is, Tuple>>(std::get<Is>(tuple)))...);
-    case 1: return std::forward<Func>(func)(makeSurface<uint8_t> (std::forward<std::tuple_element_t<Is, Tuple>>(std::get<Is>(tuple)))...);
+    case 8: return std::forward<Func>(std::get<sizeof...(Is)>(tuple))(handleArg<uint64_t>(std::forward<std::tuple_element_t<Is, Tuple>>(std::get<Is>(tuple)))...);
+    case 4: return std::forward<Func>(std::get<sizeof...(Is)>(tuple))(handleArg<uint32_t>(std::forward<std::tuple_element_t<Is, Tuple>>(std::get<Is>(tuple)))...);
+    case 2: return std::forward<Func>(std::get<sizeof...(Is)>(tuple))(handleArg<uint16_t>(std::forward<std::tuple_element_t<Is, Tuple>>(std::get<Is>(tuple)))...);
+    case 1: return std::forward<Func>(std::get<sizeof...(Is)>(tuple))(handleArg<uint8_t> (std::forward<std::tuple_element_t<Is, Tuple>>(std::get<Is>(tuple)))...);
   }
 }
 
@@ -70,7 +78,7 @@ template <typename... Args>
 
 template <typename Image, typename Func>
 [[nodiscard]] decltype(auto) visitSurface(Image &image, Func &&func) {
-  return visitSurfaces(image, func);
+  return visitSurfaces(image, std::forward<Func>(func));
 }
 
 }
