70b3ffadc9aab45ff1d7b63adc2985e68ec8bcce
Kerndog73 <kerndog73@gmail.com>
2020-03-28 18:25:03 +1030

    Pass --namespace to rcc

--- src/resources.cpp
+++ src/resources.cpp
@@ -1317,50 +1317,30 @@ static const unsigned char qt_resource_struct[] = {
 
 };
 
-#ifdef QT_NAMESPACE
-#  define QT_RCC_PREPEND_NAMESPACE(name) ::QT_NAMESPACE::name
-#  define QT_RCC_MANGLE_NAMESPACE0(x) x
-#  define QT_RCC_MANGLE_NAMESPACE1(a, b) a##_##b
-#  define QT_RCC_MANGLE_NAMESPACE2(a, b) QT_RCC_MANGLE_NAMESPACE1(a,b)
-#  define QT_RCC_MANGLE_NAMESPACE(name) QT_RCC_MANGLE_NAMESPACE2( \
-        QT_RCC_MANGLE_NAMESPACE0(name), QT_RCC_MANGLE_NAMESPACE0(QT_NAMESPACE))
-#else
-#   define QT_RCC_PREPEND_NAMESPACE(name) name
-#   define QT_RCC_MANGLE_NAMESPACE(name) name
-#endif
-
-#ifdef QT_NAMESPACE
-namespace QT_NAMESPACE {
-#endif
-
 bool qRegisterResourceData(int, const unsigned char *, const unsigned char *, const unsigned char *);
 bool qUnregisterResourceData(int, const unsigned char *, const unsigned char *, const unsigned char *);
 
-#ifdef QT_NAMESPACE
-}
-#endif
-
-int QT_RCC_MANGLE_NAMESPACE(qInitResources)();
-int QT_RCC_MANGLE_NAMESPACE(qInitResources)()
+int qInitResources();
+int qInitResources()
 {
     int version = 3;
-    QT_RCC_PREPEND_NAMESPACE(qRegisterResourceData)
+    qRegisterResourceData
         (version, qt_resource_struct, qt_resource_name, qt_resource_data);
     return 1;
 }
 
-int QT_RCC_MANGLE_NAMESPACE(qCleanupResources)();
-int QT_RCC_MANGLE_NAMESPACE(qCleanupResources)()
+int qCleanupResources();
+int qCleanupResources()
 {
     int version = 3;
-    QT_RCC_PREPEND_NAMESPACE(qUnregisterResourceData)
+    qUnregisterResourceData
        (version, qt_resource_struct, qt_resource_name, qt_resource_data);
     return 1;
 }
 
 namespace {
    struct initializer {
-       initializer() { QT_RCC_MANGLE_NAMESPACE(qInitResources)(); }
-       ~initializer() { QT_RCC_MANGLE_NAMESPACE(qCleanupResources)(); }
+       initializer() { qInitResources(); }
+       ~initializer() { qCleanupResources(); }
    } dummy;
 }
