7743fa32b4b0c59ddf35e876ddde27aac2ff5189
Kerndog73 <kerndog73@gmail.com>
2020-05-23 21:54:09 +0930

    Use envsubst

--- scripts/createdmg.sh
+++ scripts/createdmg.sh
@@ -2,10 +2,11 @@
 
 # Puts the app package into a dmg file
 
-# Requires PROJECT_DIR, BUILT_PRODUCTS_DIR, FULL_PRODUCE_NAME, 
+# Requires PROJECT_DIR, BUILT_PRODUCTS_DIR, FULL_PRODUCT_NAME, 
 # CODE_SIGN_IDENTITY, EXPANDED_CODE_SIGN_IDENTITY
 # Requires appdmg
+# Requires envsubst from gettext
 
-sed "s~\\\$BUILT_PRODUCTS_DIR~$BUILT_PRODUCTS_DIR~g;s~\\\$FULL_PRODUCT_NAME~$FULL_PRODUCT_NAME~g;s~\\\$CODE_SIGN_IDENTITY~$CODE_SIGN_IDENTITY~g;s~\\\$EXPANDED_CODE_SIGN_IDENTITY~$EXPANDED_CODE_SIGN_IDENTITY~g" "$PROJECT_DIR/scripts/dmg.json" > expanded.json
+cat "$PROJECT_DIR/scripts/dmg.json" | envsubst > expanded.json
 appdmg expanded.json "$PROJECT_DIR/Animera.dmg"
 rm expanded.json
