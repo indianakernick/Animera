3042e1ba9101bae8db30e0935254a1e533e69271
Kerndog73 <kerndog73@gmail.com>
2020-05-23 18:41:23 +0930

    Add scripts

---
+++ scripts/createico.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Creates the Windows ico file from the appiconset.
+
+# Requires PROJECT_DIR.
+# Requires icotool from icoutils
+
+ICONS="$PROJECT_DIR/Assets.xcassets/AppIcon.appiconset"
+icotool -c "$ICONS/16.png" "$ICONS/32.png" "$ICONS/64.png" "$ICONS/128.png" "$ICONS/256.png" -o "$PROJECT_DIR/Resources/Logo/icon.ico"
---
+++ scripts/exporticon.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Exports the icon.animera file to the appiconset using the Animera CLI.
+
+# Requires BUILT_PRODUCTS_DIR, EXECUTABLE_PATH and PROJECT_DIR
+# Requires that Animera be built
+
+alias animera="$BUILT_PRODUCTS_DIR/$EXECUTABLE_PATH"
+OUT="$PROJECT_DIR/Assets.xcassets/AppIcon.appiconset/"
+IN="$PROJECT_DIR/Resources/Sprites/icon.animera"
+
+function ex {
+  animera export --name="$1" --directory="$OUT" --no-composite --scale="$2" "$IN"
+}
+
+ex 16 1
+ex 32 2
+ex 64 4
+ex 128 8
+ex 256 16
+ex 512 32
+ex 1024 64
