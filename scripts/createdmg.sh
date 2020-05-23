a0f68e0bec879946a782b30fb20ae2a10d11696f
Kerndog73 <kerndog73@gmail.com>
2020-05-23 23:34:43 +0930

    Improve dmg background

--- scripts/createdmg.sh
+++ scripts/createdmg.sh
@@ -2,11 +2,20 @@
 
 # Puts the app package into a dmg file
 
-# Requires PROJECT_DIR, BUILT_PRODUCTS_DIR, FULL_PRODUCT_NAME, 
+# Requires PROJECT_DIR, BUILT_PRODUCTS_DIR, EXECUTABLE_PATH, FULL_PRODUCT_NAME, 
 # CODE_SIGN_IDENTITY, EXPANDED_CODE_SIGN_IDENTITY
 # Requires appdmg
 # Requires envsubst from gettext
 
+alias animera="$BUILT_PRODUCTS_DIR/$EXECUTABLE_PATH"
+BACKGROUND="$PROJECT_DIR/Resources/Sprites/dmg background.animera"
+
+animera export --name=background --scale=4 "$BACKGROUND"
+animera export --name=background@2x --scale=8 "$BACKGROUND"
+
 cat "$PROJECT_DIR/scripts/dmg.json" | envsubst > expanded.json
 appdmg expanded.json "$PROJECT_DIR/Animera.dmg"
+
 rm expanded.json
+rm background.png
+rm background@2x.png
