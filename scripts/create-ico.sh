212c9a64c8f1280be3cc422f16ecd2981ab989c2
Kerndog73 <kerndog73@gmail.com>
2020-07-06 13:50:06 +0930

    Generate 24px and 48px icons
    Just making sure Windows doesn't do something weird!

--- scripts/create-ico.sh
+++ scripts/create-ico.sh
@@ -4,9 +4,16 @@
 
 # Requires PROJECT_DIR
 # Requires icotool from icoutils
+# Requires gm (graphicsmagick)
 
 APPICONS="$PROJECT_DIR/Assets.xcassets/AppIcon.appiconset"
-icotool -c "$APPICONS/16.png" "$APPICONS/32.png" "$APPICONS/64.png" "$APPICONS/128.png" "$APPICONS/256.png" -o "$PROJECT_DIR/Resources/Logo/appicon.ico"
+gm convert "$APPICONS/32.png" -resize 24 24.png
+gm convert "$APPICONS/16.png" -filter Point -resize 48 48.png
+icotool -c "$APPICONS/16.png" 24.png "$APPICONS/32.png" 48.png "$APPICONS/64.png" "$APPICONS/128.png" "$APPICONS/256.png" -o "$PROJECT_DIR/Resources/Logo/appicon.ico"
+rm 24.png 48.png
 
 DOCICONS="$PROJECT_DIR/Assets.xcassets/Document.iconset"
-icotool -c "$DOCICONS/icon_16x16.png" "$DOCICONS/icon_32x32.png" "$DOCICONS/icon_32x32@2x.png" "$DOCICONS/icon_128x128.png" "$DOCICONS/icon_256x256.png" -o "$PROJECT_DIR/Resources/Logo/docicon.ico"
+gm convert "$DOCICONS/icon_32x32.png" -resize 24 24.png
+gm convert "$DOCICONS/icon_16x16.png" -filter Point -resize 48 48.png
+icotool -c "$DOCICONS/icon_16x16.png" 24.png "$DOCICONS/icon_32x32.png" 48.png "$DOCICONS/icon_32x32@2x.png" "$DOCICONS/icon_128x128.png" "$DOCICONS/icon_256x256.png" -o "$PROJECT_DIR/Resources/Logo/docicon.ico"
+rm 24.png 48.png
