44e7daca9b60794e0fc5ea84211d42e53f54312b
Kerndog73 <kerndog73@gmail.com>
2020-07-08 17:59:50 +0930

    Set bundle version

--- scripts/create-dmg.sh
+++ scripts/create-dmg.sh
@@ -3,7 +3,7 @@
 # Puts the app package into a dmg file
 
 # Requires PROJECT_DIR, BUILT_PRODUCTS_DIR, EXECUTABLE_PATH, FULL_PRODUCT_NAME, 
-# CODE_SIGN_IDENTITY, EXPANDED_CODE_SIGN_IDENTITY
+# CODE_SIGN_IDENTITY, EXPANDED_CODE_SIGN_IDENTITY, MARKETING_VERSION
 # Requires appdmg
 # Requires envsubst from gettext
 
@@ -14,7 +14,7 @@ animera export --name=background --scale=4 "$BACKGROUND"
 animera export --name=background@2x --scale=8 "$BACKGROUND"
 
 envsubst < "$PROJECT_DIR/scripts/dmg.json" > expanded.json
-appdmg expanded.json "$PROJECT_DIR/Animera.dmg"
+appdmg expanded.json "$PROJECT_DIR/Animera-$MARKETING_VERSION.dmg"
 
 rm expanded.json
 rm background.png
